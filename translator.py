c7b0331429b79161c859e5c2158cefaaa3b3cb64
Ganesh R(C) <gr@infinera.com>
2024-01-02 13:13:55 +0530

    moving to advanced topics

---
+++ translator.py
@@ -0,0 +1,13 @@
+#Giraffe Language
+#vowels ->g
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou":
+            translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Enter a phrase: ")))
